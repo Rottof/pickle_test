538fbdb5f8a7ff0f3b2d1b773cfde94ab6ec5470
rottoG <rotto333q@gmail.com>
2025-05-21 21:14:17 +0800

    项目完整代码

--- Pickle_Test/utils/hash_utils.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import hashlib
-
-def get_pickle_hash(obj, protocol=pickle.DEFAULT_PROTOCOL):
-    try:
-        data = pickle.dumps(obj, protocol=protocol)
-        return hashlib.sha256(data).hexdigest()
-    except Exception as e:
-        return f"ERROR: {e}"
